754f107df5e9868da8909357143f4ffbdc616c39
Marcello Barnaba <vjt@openssl.it>
2007-05-02 19:44:30 +0000

    implemented the whois request
    git-svn-id: https://code.softmedia.info/svn/opensource/nijacker/trunk@54 939359bc-5202-0410-adc4-d1279421971d

--- lib/whois_requester.rb
+++ lib/whois_requester.rb
@@ -1,5 +1,28 @@
+require 'socket'
+
 class WhoisRequester
-  def test
-    "sux a lot!"
+  def initialize
+    @config = YAML.load File.read('config/whois_requester.yml')
+  end
+
+  def challenge
+    true if domain_status == 'DELETED'
+  end
+
+  def shutdown
+    DRb.stop_service
+  end
+
+  protected
+  def domain_status
+    TCPSocket.open(@config['whois_server'], 43) do |sock|
+      sock.write @config['whois_domain'] + "\n"
+      response = timeout(5) { sock.read }
+      response.scan(/Status:\s*([\w_-]+)/).flatten.first
+    end
+  rescue Errno::ECONNREFUSED
+    'REFUSED'
+  rescue Timeout::Error
+    'TIMEOUT'
   end
 end

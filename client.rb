0d4aeb9652fd5ba92b778c3f6d44625be628c7bd
Marcello Barnaba <vjt@openssl.it>
2007-05-02 19:43:08 +0000

    implemented a round-robin request for each configured node, shutdown of every node and fax delivery upon completion.
    git-svn-id: https://code.softmedia.info/svn/opensource/nijacker/trunk@52 939359bc-5202-0410-adc4-d1279421971d

--- client.rb
+++ client.rb
@@ -1,15 +1,58 @@
 #!/usr/bin/ruby
 
 require 'drb/drb'
+require 'lib/mailer'
+class Fixnum
+  def minutes; self * 60; end
+  alias :minute :minutes
+end
 
-uri_list = File.read('config/server.list').split($/)
-puts "configured #{uri_list.size} servers"
+class Client
+  def initialize
+    @uri_list = File.read('config/server.list').split($/)
+    @holdtime = 30.minutes / @uri_list.size
+    puts "configured #{@uri_list.size} servers, holdtime #{@holdtime}s"
 
-DRb.start_service
-
-#loop do
-  uri_list.each do |uri|
-    server = DRbObject.new_with_uri(uri)
-    puts "server at #{uri} says: #{server.test}"
+    DRb.start_service
   end
-#end
+
+  def run
+    catch :mission_accomplished do
+      try_every_server_in_list
+    end
+
+    each_server do |server, uri|
+      puts "shutting down #{uri}" 
+      server.shutdown
+    end
+
+    puts 'delivering FAX...'
+    Mailer.deliver_fax
+    exit
+  end
+
+  protected
+  def try_every_server_in_list
+    loop do
+      each_server do |server, uri|
+        if server.challenge
+          puts "finished!"
+          throw :mission_accomplished
+        end
+
+        puts "[#{Time.now}] still trying (last tried: #{uri})"
+        sleep @holdtime
+      end
+    end
+  end
+
+  def each_server
+    @uri_list.each do |uri|
+      yield DRbObject.new_with_uri(uri), uri
+    end
+  end
+end
+
+if __FILE__ == $0
+  Client.new.run
+end

01fbf9c985ebb54fef64a88347a407f7b299bca1
Marcello Barnaba <vjt@openssl.it>
2007-05-02 19:40:56 +0000

    fake whois server, useful for tests.
    git-svn-id: https://code.softmedia.info/svn/opensource/nijacker/trunk@51 939359bc-5202-0410-adc4-d1279421971d

---
+++ lib/fake_whois_server.rb
@@ -0,0 +1,30 @@
+require 'socket'
+
+class WhoisServer
+  include Socket::Constants
+
+  def initialize
+    @sock = TCPServer.new(43)
+    trap('TERM') { exit }
+  end
+
+  def run
+    loop do
+      client, client_addr = @sock.accept
+      request = client.readline
+      client.write answer(request)
+      client.close
+    end
+  end
+
+  protected
+  def answer(domain)
+    text = File.read 'config/fake_whois_answer.txt'
+    text.sub! '@@DOMAIN@@', domain
+    text.sub! '@@STATUS@@', (rand(10) != 3) ? 'ACTIVE' : 'DELETED'
+  end
+end
+
+if __FILE__ == $0
+  WhoisServer.new.run
+end
